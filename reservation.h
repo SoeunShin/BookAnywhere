38d6d6ee9c0d9a22011cfbc79b8308641df53002
KateKim <61180744+greenyeast@users.noreply.github.com>
2021-05-03 21:44:55 +0900

    Update reservation.h

--- reservation.h
+++ reservation.h
@@ -1,3 +1,8 @@
+int addUser(User *u); //사용자 계정 만들기
+void timeRemain(User u); //잔여시간 출력
+void readHistory(User u);  //file에 저장된 히스토리 읽기
+//int update(Score *s1);  //시간 업데이트
+void searchUser(User *u);  //사용자이용정보 검색
 void readUser(User u); // 사용자 목록 조회
 int deleteUser(User *u); // 사용자 목록 삭제
 void searchUsing(User *u, int cnt); // 사용중인 자리 검색
