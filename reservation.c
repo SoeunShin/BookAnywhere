a999398435374f6e757a8a1ecd444533f7d44f32
SoeunShin <21900395@handong.edu>
2021-05-06 22:59:30 +0900

    added functions

--- reservation.c
+++ reservation.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include "reservation.h"
+
+void readUser(User u){	// 사용자 목록 조회
+
+}
+int deleteUser(User *u){ // 사용자 목록 삭제
+
+}
+void searchUsing(User *u, int cnt){ // 사용중인 자리 검색
+
+}
+void searchEmpty(User *u, int cnt){ // 비어있는 자리 검색
+
+}	
+void saveFile(User *u, int cnt){ // 파일 저장
+
+}
