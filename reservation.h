2d361a24da39750c7aa1092103a31a9def5dddf2
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2021-05-09 15:17:15 +0900

    Update reservation.h

--- reservation.h
+++ reservation.h
@@ -5,14 +5,15 @@ typedef struct{
   int seat; 
 } User;
 
-int addUser(User *u); //사용자 계정 만들기
-void usingTime(User u); //사용시간 출력
-void readHistory(User u);  //file에 저장된 히스토리 읽기
-int updateSeat(User *u);  //자리 상태 업데이트
-void searchUser(User *u);  //사용자이용정보 검색
+int addUser(User *u); // 사용자 계정 만들기
+void usingTime(User u); // 사용시간 출력
+void readHistory(User u); // file에 저장된 히스토리 읽기
+int updateSeat(User *u); // 자리 상태 업데이트
+void searchUser(User *u); // 사용자이용정보 검색
 void readUser(User u); // 사용자 목록 조회
 int deleteUser(User *u); // 사용자 목록 삭제
-void checkinout(User *u);  //입퇴실 처리
+void checkinout(User *u); //입퇴실 처리
 void searchUsing(User *u, int cnt); // 사용중인 자리 검색
 void searchEmpty(User *u, int cnt); // 비어있는 자리 검색
+int selectMenu(); // 메뉴 선택
 void saveFile(User *u, int cnt); // 파일 저장
