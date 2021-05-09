07fa23267d908908c36580d9b4ec4e4b890cde58
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2021-05-09 15:00:02 +0900

    Update reservation.c

--- reservation.c
+++ reservation.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
-#include "reservation.h"
+#include <time.h>
+#include "oss_team.h"
 
-void readUser(User u){	// 사용자 목록 조회
-
+void readUser(User u){  // 사용자 목록 조회
+        printf("%30s%20s%20s%10d\n", u.id, u.in, u.out, u.seat);
 }
 int deleteUser(User *u){ // 사용자 목록 삭제
-
+        u->in = "";
+        u->out = "";
+        u->seat = -1;
 }
+void checkinout(User *u){  //입퇴실 처리
+        char answer;
+        time_t checkin;
+        time_t checkout;
+        printf("Please enter I(In) or O(Out): ");
+        scanf("%c", answer);
+        
+        if (answer == 'I'){
+                time(&checkin);
+        }
+        if (answer == 'O'){
+                time(&checkout);
+        }
+        else{
+                printf("Cancelled!\n");
+        }
+}
+/*
 void searchUsing(User *u, int cnt){ // 사용중인 자리 검색
 
 }
 void searchEmpty(User *u, int cnt){ // 비어있는 자리 검색
 
-}	
+}  
 void saveFile(User *u, int cnt){ // 파일 저장
 
 }
-
-
+*/
