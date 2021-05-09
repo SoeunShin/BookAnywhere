4fe289c66d5b13e29170ca6e4678a28c60451bff
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2021-05-09 15:52:49 +0900

    Update reservation.h

--- reservation.h
+++ reservation.h
@@ -13,6 +13,7 @@ void searchUser(User *u); // 사용자 검색
 void readUser(User u); // 사용자 목록 조회
 int deleteUser(User *u); // 사용자 목록 삭제
 void checkinout(User *u); // 입퇴실 처리
+int selectNo(); // 번호 선택 
 void searchUsing(User *u, int cnt); // 사용중인 자리 검색
 void searchEmpty(User *u, int cnt); // 비어있는 자리 검색
 int selectMenu(); // 메뉴 선택
