f1a94518371af39829b9e430b57858e2e1f3c3a0
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2021-05-10 13:49:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,23 @@
 
 int main(void){
     User ulist[30];
-    int index = 0, count = 0, menu;
+    int index = 0, cnt = 0, menu;
+
+    for (int i=0; i<30; i++){
+        ulist[i].in = 0;
+        ulist[i].out = 0;
+        ulist[i].seat = 0;
+    }
 
     while(1){
         menu = selectMenu();
         if (menu == 0) break; // 0: 종료
         if (menu == 1){ // 1: 사용자 계정 생성
-            count += addUser(&ulist[index]);
+            cnt += addUser(&ulist[index]);
             index++;
         }
         else if (menu == 2){ // 2: 입퇴실 신청 
-            int no = selectNo(ulist, index);
+            int no = selectNo(ulist);
             if (no == 0){
                 printf("Cancelled!\n");
                 continue;
@@ -21,16 +27,32 @@ int main(void){
             checkinout(&ulist[no-1]);
         }
         else if (menu == 3){ // 3: 현재 사용자 전체 목록 조회
-            for (int i=0; i<30; i++){
-                printf("%d's user: ",i+1);
-                readUser(ulist[i]);
-            }
+            listUser(ulist);
         }
         else if (menu == 4){  // 4: 사용자 목록 삭제
-            int userIndex;
-            printf("which user do you want to delete?(input a user index number) ");
-            scanf("%d", &userIndex);
-            deleteUser(&ulist[userIndex]);
+            int no = selectNo(ulist);
+            if (no == 0){
+                printf("Cancelled!\n");
+                continue;
+            }
+            int delok;
+            printf("Do you want to delete it?(Y: Yes) ");
+            scanf("%c", delok);
+            if (delok == 'Y'){
+                deleteUser(&ulist[no-1]);
+            }
+            else{
+                pirntf("Cancelled!\n");
+                continue;
+            }
+        }
+        else if (menu == 5){ // 5: 사용시간 조회
+
+        }
+        else if (menu == 6){ // 6: 예약 수정
+
         }
     }
+    printf("EXIT\n");
+    return 0;
 }
