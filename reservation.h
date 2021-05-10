8f20853456c0cef439365fe38651a241d612756e
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2021-05-10 15:06:07 +0900

    Update reservation.h

--- reservation.h
+++ reservation.h
@@ -1,5 +1,5 @@
 typedef struct{ 
-  char id[20]; 
+  char id[20];
   int in;
   int out;
   int seat; 
@@ -14,7 +14,7 @@ void readUser(User u); // 사용자 한 명 목록 출력
 int deleteUser(User *u); // 사용자 목록 삭제
 void checkinout(User *u);  // 입퇴실 처리
 int selectNo(User u[]); // 번호 선택 
-int listUser(User *u); // 전체 목록 출력
+void listUser(User *u); // 전체 목록 출력
 void searchUsing(User *u, int cnt); // 사용중인 자리 검색
 void searchEmpty(User *u, int cnt); // 비어있는 자리 검색
 int selectMenu(); // 메뉴 선택
