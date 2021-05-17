306e0c0aab60d94a69823466ff93cb68eab0a9b1
GayoungKim <61180744+greenyeast@users.noreply.github.com>
2021-05-18 00:29:53 +0900

    Update reservation.h

--- reservation.h
+++ reservation.h
@@ -8,9 +8,9 @@ typedef struct{
 int addUser(User *u); // 사용자 계정 생성
 void usingTime(User u); // 사용시간 출력
 void timeRemain(User u); // 잔여시간 출력
-void readHistory(User u);  // 파일에 저장된 히스토리 읽기
+void readHistory(User *u);  // 파일에 저장된 히스토리 읽기
 void updateSeat(User *u);  // 자리 상태 업데이트
-void searchUser(User *u);  // 사용자 이용 정보 검색
+void searchUser(User *u, int cnt);  // 사용자 이용 정보 검색
 void readUser(User u); // 사용자 한 명 목록 출력
 int deleteUser(User *u); // 사용자 목록 삭제
 void checkinout(User *u);  // 입퇴실 처리
@@ -20,4 +20,4 @@ void searchUsing(User *u, int cnt); // 사용중인 자리 조회
 void searchEmpty(User *u, int cnt); // 빈 자리 검색
 int selectMenu(); // 메뉴 선택
 void saveFile(User *u, int cnt); // 파일 저장
-int checkSeat(User *u); // 자리 상태 확인
+int checkSeat(User u[], int select); //자리 상태 확인
